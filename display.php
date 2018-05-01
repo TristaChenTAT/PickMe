9277a7fa97fa3f502bb6986ec431aa61c705ebd0
SDEKLNSD <38280570+SDEKLNSD@users.noreply.github.com>
2018-05-02 01:13:57 +0800

    Add files via upload

---
+++ display.php
@@ -0,0 +1,27 @@
+<?php
+
+	$servername = "localhost";//連接伺服器
+	$username = "root";
+	$password = "123";
+	$dbname = "groupsix_db";
+	$conn = new mysqli($servername, $username, $password, $dbname);//create connection
+	mysqli_query($conn, "SET NAMES 'UTF8'");
+	$sql=sprintf("select * from clothes ORDER BY cnum");
+	$result=$conn->query($sql);
+?>
+<HTML>
+<BODY>
+
+	<?php
+	while($row = mysqli_fetch_array($result)) {
+	?>
+	<img src="image.php?cnum=<?php echo $row["cnum"]; ?>" /><br/>
+	<?php
+	}
+?>
+</BODY>
+</HTML>
+
+
+
+
---
+++ image.php
@@ -0,0 +1,20 @@
+<?php
+    
+    //從資料庫取得圖片
+    $servername = "localhost";//連接伺服器
+	$username = "root";
+	$password = "123";
+	$dbname = "groupsix_db";//選擇欲讀取的資料庫名稱
+		
+	$conn = new mysqli($servername, $username, $password, $dbname);//create connection
+	mysqli_query($conn, "SET NAMES 'UTF8'");
+      
+	if(isset($_GET['cnum'])){	
+		$sql=sprintf("select * from clothes where cnum=". $_GET['cnum']);        
+		$result=$conn->query($sql);        
+		$row = mysqli_fetch_array($result);    
+        header("Content-type: image/jpeg");     
+        echo $row['image']; 		
+    }
+
+?>
